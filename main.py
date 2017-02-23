ad3c9aea24a143979f1a5487b4227917b369aa98
kirill-galushko <galushko.ka@gmail.com>
2017-02-23 14:12:57 +0300

    Fixes

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ import numpy as np
 import pandas
 data = pandas.read_csv('titanic.csv', index_col='PassengerId')
 
-print ("\n1st question")
-print data['Sex'].value_counts()
 
-print ("\n2nd question")
+print ("\n1st task")
+print data['Sex'].value_counts().values
+
+print ("\n2nd task")
 a = data['Survived'].loc[data['Survived'] == 1].value_counts().values
 print np.round(np.true_divide(a, data.__len__())*100,2)
 
-print ("\n3th question")
+print ("\n3th task")
 a = data['Pclass'].loc[data['Pclass'] == 1].value_counts().values
 print np.round(np.true_divide(a, data.__len__())*100,2)
 
-print ("\n4th question")
-print data['Age'].mean()
-print data['Age'].median()
+print ("\n4th task")
+print np.round(data['Age'].mean(),2)
+print np.round(data['Age'].median(),2)
 
-print ("\n5th question")
-print data[['SibSp','Parch']].corr('pearson')
+print ("\n5th task")
+print np.round(data[['SibSp','Parch']].corr('pearson').values[1,0],2)
 
-print ("\n6th question")
-print data['Name'].loc[data['Sex'] == 'female'].str.split('.').str[1].str.split(' ').str[1].str.replace('[(,)]','').value_counts().head(1)
+print ("\n6th task")
+print data['Name'].loc[data['Sex'] == 'female'].str.split('.').str[1].str.split('(').str[-1].str.split(' ').str[1].str.replace('[(,)]','').value_counts().head(1)
